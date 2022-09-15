7e20a00a67d67cc1f4df3cbd174465dc2139a21f
LucianoMariotti <luciano.mariotti99@gmail.com>
2022-09-15 01:23:47 -0300

    add href to navbar bottom

--- components/Layout/MobileBottomNav/index.js
+++ components/Layout/MobileBottomNav/index.js
@@ -1,20 +1,28 @@
 import { Box, Button, Flex, Text } from "@chakra-ui/react";
+import Link from "next/link";
 import React from "react";
 import Home from "../../Icons/Home";
-import Save from "../../Icons/Save";
 import User from "../../Icons/User";
 
 export default function MobileBottomNavbar() {
   return (
     <>
       <Flex layerStyle="mobilenav">
-        <Home />
+        <Link href="/">
+          <Box cursor="pointer">
+            <Home />
+          </Box>
+        </Link>
         <Button bg="brand.100" borderRadius={999} h={10} w={10}>
           <Text fontWeight="800" fontSize="20px" textAlign="center">
             +
           </Text>
         </Button>
-        <User />
+        <Link href="/Profile">
+          <Box cursor="pointer">
+            <User />
+          </Box>
+        </Link>
       </Flex>
     </>
   );
